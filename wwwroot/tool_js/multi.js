7521635e64b7e7a9af44947b748a212a9f60472b
kermit <kermit1215@gmail.com>
2025-06-02 17:57:10 +0800

    重構LoanSky Jason檔讀取try catch

---
+++ wwwroot/tool_js/multi.js
@@ -0,0 +1,40 @@
+﻿var $activeText;
+$(function() {
+	$("#tags").autocomplete({
+		source: function(request, response)
+			{
+				$.getJSON('ac-ajax.cfm', {term: request.term}, response);
+			},
+		search: function() 
+			{
+				var term = this.value;
+				if (term.length < 2) 
+					return false;
+			},
+		focus: function() 
+			{
+				return false;
+			},
+		select:function(event, ui)
+			{
+				$activeText.val(ui.item.label).css({'visibility':'visible'});
+				$(this).val('').css({'visibility':'hidden'});
+				return false;
+			}
+	})
+});
+$(document).ready(function(){
+	$('.txt').click(function(){
+		$activeText = $(this);
+		$(this).css({'visibility':'hidden'});
+		$('#tags').val($(this).val()).css({'visibility':'visible', 'left':$(this).position().left, 'top':$(this).position().top}).select().focus();
+	});
+	$('#tags').blur(function(){
+		if($activeText != null)
+		{
+			$activeText.css({'visibility':'visible'});
+			$activeText.val($(this).val());
+		}
+		$(this).css({'visibility':'hidden'});
+	});
+});
---
+++ wwwroot/tool_js/single.js
@@ -0,0 +1,23 @@
+﻿$(function() {
+	$("#txt").autocomplete({
+		source: function(request, response)
+			{
+				$.getJSON('ac-ajax.cfm', {term: request.term}, response);
+			},
+		search: function() 
+			{
+				var term = this.value;
+				if (term.length < 2) 
+					return false;
+			},
+		focus: function() 
+			{
+				return false;
+			},
+		select:function(event, ui)
+			{
+				$('#txt').val(ui.item.label);
+				return false;
+			}
+	})
+});
